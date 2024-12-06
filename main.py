85d4633ba10e3e86aab90bfe09d7b3be4880505d
ST-BBG <trunkstefan1@gmail.com>
2024-12-06 20:59:07 +0100

    CH2/6 done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 from constants import *
-from player import *
+from player import Player
 
 fps = pygame.time.Clock()
 dt = 0
@@ -12,7 +12,7 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
 player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
 
 def game_loop():
@@ -24,8 +24,8 @@ def game_loop():
         pygame.Surface.fill(screen, (0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
-        fps.tick(60)
-    
+        dt = fps.tick(60) / 1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
